04fec07fa89fdff1b90b68fc2ed8cd73702e230e
dVera17 <dsvera174@gmail.com>
2023-07-11 23:04:38 -0500

    feat: :sparkles: Se agrega un parametro a la condiguracion de la conexion a la bd
    Se agrega un parametro a la condiguracion de la conexion a la bd

--- src/db/database.js
+++ src/db/database.js
@@ -5,7 +5,8 @@ const conn = mysql.createPool({
     host: config.host,
     user: config.user,
     password: config.password,
-    database: config.database
+    database: config.database,
+    multipleStatements: true
 });
 
 const getConn = () => {
