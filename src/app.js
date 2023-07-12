49dfc24f8bf355449d97441ebb533b445ad94b63
dVera17 <dsvera174@gmail.com>
2023-07-11 22:09:33 -0500

    feat: :sparkles: se hace funcional el endpoint inventarios para poderce utilizar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodegasRouter from "./routes/bodegas.routes.js";
 import productosRouter from "./routes/productos.routes.js";
+import inventariosRouter from "./routes/inventarios.routes.js";
 
 const app = express();
 app.set('port', 5010);
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/bodegas', bodegasRouter);
 app.use('/productos', productosRouter);
+app.use('/inventarios', inventariosRouter);
 
 export default app;
